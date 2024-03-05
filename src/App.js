87cc39ea94d52f6cd443e15d072ff7c6e069bf74
Arvind <arvindnagalbone@gmail.com>
2024-03-05 09:46:01 +0530

    chnaged App file

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = (props) => {
       >
         Count
       </button>
-      <h1>Name: {props.name}</h1>
-      <h1>Age: {props.age}</h1>
+      <h1>Name1: {props.name}</h1>
+      <h1>Age1: {props.age}</h1>
     </div>
   );
 };
